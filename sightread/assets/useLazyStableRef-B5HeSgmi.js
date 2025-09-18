ad5d1cbc4405118a60dd81604e9b37a64c467039
cubiculociencias <piano.unam@gmail.com>
2025-09-17 18:08:24 -0600

    Add files via upload

---
+++ sightread/assets/useLazyStableRef-B5HeSgmi.js
@@ -0,0 +1,1 @@
+import{a as t}from"./chunk-PVWAREVJ-BeAWct5i.js";function n(e){const r=t.useRef(null);return r.current||(r.current=e()),r.current}export{n as u};
---
+++ sightread/assets/useWakeLock-DSwNU-kK.js
@@ -0,0 +1,6 @@
+import{c as o}from"./Dropdown-B9h-t97s.js";import{h as c,j as e}from"./Canvas-C5qiJpk2.js";import{a as r}from"./chunk-PVWAREVJ-BeAWct5i.js";/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]],u=o("chevron-up",t);function p(){r.useEffect(()=>(c(),()=>e()),[])}export{u as C,p as u};
